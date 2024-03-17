4f389f5af12336be08318173d362aa24aa56f343
ars <163355758+Arsdwg@users.noreply.github.com>
2024-03-17 12:15:43 +0600

    first

---
+++ lesson7.py
@@ -0,0 +1,35 @@
+# lambda  функция и работа с файлами
+
+# def upletter(word:str):
+#     return word.title()
+
+# def showwords(func, it):
+#     for i in it:
+#         print(func(i))
+
+
+
+# cities = ['tokmok ' , 'karakol' , 'bish']
+# showwords(upletter, cities)
+# showwords(len, ['tokmok ' , 'karakol' , 'bish'])
+
+# showwords(lambda word: word.title() ,cities)
+# numbers = list(range(1,16))
+# print(numbers)
+# filtered = list(filter(lambda number : number > 10,numbers))
+# print(filtered)
+# filter - отбирает объекты но не изменяет
+# map - изменяет каждый объеект
+# mapped = tuple(map(lambda num: num / 2,numbers))
+# print(mapped)
+
+# countries = {
+#     'chuy':7,
+#     'osh':6,
+#     'jjj':9
+# }
+# sortedcoun = sorted(countries , key=lambda x: x[1])
+# (sortedcoun)
+# print
+
+
